0c068d28c143bd7f7834cd0a0f8f99e21b8a5af3
Eric Spencer <spencer22eric@gmail.com>
2022-06-28 18:41:31 -0400

    import bootstrap/initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <head>
+        <title>Portfolio</title>
+        
+      </head>
+      <header>
+        <h1>Eric Spencer</h1>
+        <button className='btn-danger'>button</button>
       </header>
-    </div>
+    </>
   );
 }
 
