994dbba5629d0d6f457abdae1a7c377279fbdb66
Eric Spencer <spencer22eric@gmail.com>
2022-07-06 22:50:15 -0400

    contact form/styling and about styling adjustments

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,9 +8,9 @@ export default function About() {
                     <h1>About Me</h1>
                 </div>
             <div className='container-fluid d-flex'>
-                <div className='row align-items-center justify-content-center padding'>
+                <div className='row align-items-center justify-content-center padding-top'>
                     <div className='col-lg-3 m-5 justify-content-end'>
-                        <img src={image} alt='empty placeholder portrait' className='img-fluid' height='200px' />
+                        <img src={image} alt='empty placeholder portrait' className='img-fluid portrait' />
                     </div>
                     <div className='col-sm-12 col-lg-8'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div>
             <div className='col-sm-12 hero-image pt-5'>
-                <h1>Home Page</h1>
+                <h1>My Projects</h1>
             </div>
             <div className='container-fluid'>
                 <div className='row'>
